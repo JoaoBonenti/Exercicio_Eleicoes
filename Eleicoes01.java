3bce0eb3294c2c6c5f20037d7e5cb54b050dce97
Joao Guilherme Bonenti Nobrega <69513119+JoaoBonenti@users.noreply.github.com>
2022-05-23 23:07:59 -0300

    Update Eleicoes01.java

--- Eleicoes01.java
+++ Eleicoes01.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 public class Eleicoes01 {
  public static void main(String[] args) {
+  //Uso do Scanner para entrada de dados
   Scanner scan = new Scanner(System.in);
+  //Declaração das variáveis
   int numEleitor = 0;
   int PT=0, PDT=0, PL=0, PSDB=0, nulo=0, branco=0;
   
+  //Uso do For para contagem e repetição do programa
   for(int i = 0; i < 50; i++)  {
+    //Contador
     numEleitor++;
     System.out.println("\nEleitor " + numEleitor +", digite o numero do candidato a sua escolha.");
+    //Declaração e entrada de dados da variável numero
     System.out.print("Digite '13' para PT, '12' para PDT, '22' para PL, '45' para PSDB, '0' para voto Nulo, ou digite '9' para voto em branco!\nSua escolha e: ");
     int numero = scan.nextInt();
 
+   //Uso do Switch para determinar e organizar casos distintos, e também somar aos contadores
    switch (numero){
      case 13:
        System.out.println("Voce votou no Candito do partido PT!");
@@ -41,6 +47,7 @@ public class Eleicoes01 {
 
     }
   }
+    //Apresentação para o usuário dos valores agrupados e devidamente formatados
     System.out.printf("\nO total de votos para o PT foi igual a: " + PT + "!");
     System.out.printf("\nO total de votos para o PDT foi igual a: " + PDT + "!");
     System.out.printf("\nO total de votos para o PL foi igual a: " + PL + "!");
